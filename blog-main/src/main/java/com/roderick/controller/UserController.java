1fde9bbfbd74fd17cb39286a1ce961ca2f57b137
RoderickXiang <3097934536@qq.com>
2020-12-07 20:08:28 +0800

    用户信息页面（同步）✔

--- blog-main/src/main/java/com/roderick/controller/UserController.java
+++ blog-main/src/main/java/com/roderick/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.roderick.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.roderick.pojo.Article;
 import com.roderick.pojo.User;
 import com.roderick.service.ArticleService;
 import com.roderick.service.UserService;
+import com.roderick.util.PageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +19,7 @@ public class UserController {
 
     UserService userService;
     ArticleService articleService;
+    PageUtil pageUtil;
 
     @Autowired
     public void setUserService(UserService userService) {
@@ -27,18 +31,32 @@ public class UserController {
         this.articleService = articleService;
     }
 
+    @Autowired
+    public void setPageUtil(PageUtil pageUtil) {
+        this.pageUtil = pageUtil;
+    }
+
     /**
      * 用户信息页面，包含文章、问答之类的模块
      *
-     * @param uid 用户uid
+     * @param uid  用户uid
+     * @param page 页码
+     * @param size 每页信息的条数 default 20
      * @return 用户信息页面
      */
-    @GetMapping("/{uid}")
-    public String userInfoPage(@PathVariable String uid, Model model) {
+    @GetMapping({"/{uid}/article/{page}/{size}", "/{uid}/article"})
+    public String userInfoPage(@PathVariable String uid,
+                               @PathVariable(required = false) Integer page,
+                               @PathVariable(required = false) Integer size,
+                               Model model) {
         User user = userService.getUserByUid(uid);
-        user.setPosts(userService.getPostsByUid(uid));
         user.setPassword("");   //清除密码
-        model.addAttribute("user", user);
-        return "user/info";
+        Page<Article> pageArticle = articleService.getArticleListByPageAndUidOrderByTime(uid, page, size);
+
+        model.addAttribute("user", user);   //用户对象
+        model.addAttribute("pageArticle", pageArticle); //页面对象
+        model.addAttribute("totalPage",
+                pageUtil.getTotalPage(pageArticle.getTotal(), pageArticle.getSize()));  //页面总数用于做分页
+        return "user/info-article";
     }
 }
